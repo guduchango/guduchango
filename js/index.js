09e895df400e44887a4299ea636fdcc80ce4160b
Edgardo Ponce <ed.poncepaez@gmail.com>
2024-03-14 10:19:26 -0300

    add javascript magic

--- js/index.js
+++ js/index.js
@@ -3,11 +3,9 @@ var div_aboutme = document.getElementById('about-me');
 var div_experience = document.getElementById('experience');
 var div_education = document.getElementById('education');
 var div_proyects = document.getElementById('proyects');
+var main = document.querySelector('main');
 
-div_proyects.style.display = 'block';
-div_aboutme.style.display = 'none';
-div_experience.style.display = 'none';
-div_education.style.display = 'none';
+console.log(main.attributes)
 
 document.getElementById('btn-about-me').addEventListener('click', function () {
     div_aboutme.style.display = 'block';
@@ -37,9 +35,39 @@ document.getElementById('btn-proyects').addEventListener('click', function () {
     div_education.style.display = 'none';
 });
 
-/*
-about-me
-experience
-education
-proyects
-*/
+/* Mobile logical */
+
+document.getElementById('mobileHomeIcon-btn').addEventListener('click', function () {
+    div_aboutme.style.display = 'block';
+    main.style.display = 'block';
+    div_experience.style.display = 'none';
+    div_education.style.display = 'none';
+    div_proyects.style.display = 'none';
+});
+
+document.getElementById('experienceHomeIcon-btn').addEventListener('click', function () {
+    div_aboutme.style.display = 'none';
+    main.style.display = 'none';
+    div_experience.style.display = 'block';
+    div_education.style.display = 'none';
+    div_proyects.style.display = 'none';
+});
+
+document.getElementById('educationHomeIcon-btn').addEventListener('click', function () {
+    div_aboutme.style.display = 'none';
+    main.style.display = 'none';
+    div_experience.style.display = 'none';
+    div_education.style.display = 'block';
+    div_proyects.style.display = 'none';
+});
+
+document.getElementById('projectsHomeIcon-btn').addEventListener('click', function () {
+    div_aboutme.style.display = 'none';
+    main.style.display = 'none';
+    div_experience.style.display = 'none';
+    div_education.style.display = 'none';
+    div_proyects.style.display = 'block';
+});
+
+
+
