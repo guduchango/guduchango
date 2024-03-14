6d8aa1063b1d1ed13710827c00272d209030145d
Edgardo Ponce <ed.poncepaez@gmail.com>
2024-03-14 11:00:48 -0300

    add favicon pictures minimal details

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ console.log(main.attributes)
 
 document.getElementById('btn-about-me').addEventListener('click', function () {
     div_aboutme.style.display = 'block';
+    main.style.display = 'block';
     div_experience.style.display = 'none';
     div_education.style.display = 'none';
     div_proyects.style.display = 'none';
@@ -16,6 +17,7 @@ document.getElementById('btn-about-me').addEventListener('click', function () {
 
 document.getElementById('btn-experience').addEventListener('click', function () {
     div_experience.style.display = 'block';
+    main.style.display = 'block';
     div_aboutme.style.display = 'none';
     div_education.style.display = 'none';
     div_proyects.style.display = 'none';
@@ -23,6 +25,7 @@ document.getElementById('btn-experience').addEventListener('click', function () 
 
 document.getElementById('btn-education').addEventListener('click', function () {
     div_education.style.display = 'block';
+    main.style.display = 'block';
     div_aboutme.style.display = 'none';
     div_experience.style.display = 'none';
     div_proyects.style.display = 'none';
@@ -30,6 +33,7 @@ document.getElementById('btn-education').addEventListener('click', function () {
 
 document.getElementById('btn-proyects').addEventListener('click', function () {
     div_proyects.style.display = 'block';
+    main.style.display = 'block';
     div_aboutme.style.display = 'none';
     div_experience.style.display = 'none';
     div_education.style.display = 'none';
